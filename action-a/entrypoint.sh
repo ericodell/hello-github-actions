fde91080189ccf5afb185b8183fd9b59c39c338a
Eric Odell <eric.odell@ucop.edu>
2020-08-13 10:22:59 -0700

    pwd and ls

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,4 +2,5 @@
 
 sh -c "echo;echo my environment variables are;echo;env;echo;echo good bye"
 sh -c "echo;echo my disk space is;echo;df -h;echo;echo last logins;last;echo"
+sh -c "echo;my current working directory;echo;pwd;echo;ls -la;echo"
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
