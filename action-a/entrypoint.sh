b3fa7dbd949177d5c391b4dfc95f978ac49fef41
Eric Odell <eric.odell@ucop.edu>
2020-08-13 09:28:14 -0700

    env vars syntax error fix

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo;my environment variables are;echo;env;echo;echo good bye"
+sh -c "echo;echo my environment variables are;echo;env;echo;echo good bye"
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
