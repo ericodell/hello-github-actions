0ff366c4f8d5384326f15a88822362df1d421c72
Eric Odell <eric.odell@ucop.edu>
2020-08-13 09:46:45 -0700

    df and last

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
 sh -c "echo;echo my environment variables are;echo;env;echo;echo good bye"
+sh -c "echo;echo my disk space is;echo;df -h;echo;echo last logins;last;echo"
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
